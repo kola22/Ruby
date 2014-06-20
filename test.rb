610b87ce86874fa9a7f37cf496337f952c6296eb
piletskiy <piletskiy@nodasoft.com>
2014-06-20 16:09:51 +0400

    2

--- test.rb
+++ test.rb
@@ -4,25 +4,11 @@
 
 ## puts "Rubydev".reverse.tap{ |o| puts "reversed: #{o}" }.upcase
 
-my_number = 123
-puts "В двоичном виде: %b" % my_number #=> В двоичном виде: 1111011
+def method
+    10 / 0
 
-gets
-
-def method_with_block(hello)
-    yield(hello)
-end
-method_with_block("Hello!"){|msg| puts 'dsfsdfsdfsfsdfsdfsdf'+msg} #Hello
-gets
-
-def twice
- x = 66
- y = 666
- z =4
-        yield z
+rescue
+    puts 'Fuck! Divider is zero!'
 end
 
-twice { |d| if d>3
-    puts d
-end}
-
+method
