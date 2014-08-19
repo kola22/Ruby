d0f5eec53927cf7000321e49669f9e7eb7df0aee
piletskiy <piletskiy@nodasoft.com>
2014-08-19 16:54:56 +0400

    fix add order

--- _testSuite.rb
+++ _testSuite.rb
@@ -44,11 +44,7 @@ while Time.now.year < 2018
             autArr = ['piletskiy', 'nodakola22', 'piletskiy.abcp.ru']
             autArr4mc = ['piletskiy', 'nodakola22', '4mycar.ru']
             autArrAutotest = ['piletskiy', 'nodakola22', 'autotestvirtual']
-            startTestaddFranch autArr
-            startTestaddOrderFrtoGk @nameCity, 'OC90', 'Knecht', autArr
-            localText autArrAutotest,'Гуково'
-##        verifPriceUp autArr,'bHvwEaTestDel_31_7_2014'
-            if @lan == false
+            if !@lan
                 forMcOtziviShop autArr4mc
                 forMcOtzivi autArr4mc
             end
